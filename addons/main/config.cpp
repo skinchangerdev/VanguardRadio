4f71a7babd4e04480ad45c933c14cac5061af7cc
SkinChangerDev <git@skinchanger.dev>
2026-08-17 23:01:07 +0000

    chore: init

---
+++ addons/main/config.cpp
@@ -0,0 +1,15 @@
+// This file is released into the public domain. See COPYING.
+
+class CfgPatches {
+    class Main {
+        name = "Vanguard Radio - Main";
+        author = "SkinChangerDev";
+        url = "";
+
+        requiredVersion = 2.22;
+        requiredAddons[] = {"CBA_main"};
+
+        units[] = {};
+        weapons[] = {};
+    };
+};
